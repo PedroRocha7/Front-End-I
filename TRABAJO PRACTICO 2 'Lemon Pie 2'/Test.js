0b17959ae79d6a5ff499818887a396fa6e237705
PedroRocha7 <Pedrom.rocha7@gmail.com>
2021-09-03 10:26:15 -0300

    Subo mi repo front

---
+++ TRABAJO PRACTICO 2 'Lemon Pie 2'/Test.js
@@ -0,0 +1,102 @@
+// Suma
+
+console.log(8 + 9);
+
+// Resta
+
+console.log( 8 - 4);
+
+// Multiplicacion
+
+console.log(7*4);
+
+// Division
+
+console.log (10/2);
+
+// Modulo
+
+console.log (42 % 3);
+
+// Incremento 
+
+let Numero = 10
+
+console.log (++ Numero)
+
+// Decremento 
+
+let OtroNumero = 10 
+
+console.log(-- OtroNumero)
+
+// Comparacion Simple
+
+console.log (5 == '5')
+
+// Comparacion Estricta
+
+console.log (5 === '5')
+
+// Desigualdad Simple
+
+console.log (5 != '5')
+
+// Desigualdad Estricta
+
+console.log (5 !== '5')
+
+// Mayor que
+
+console.log( 10 > 6)
+
+console.log (10 > 10)
+
+// Mayor o Igual
+
+console.log (10 >= 10)
+
+// Menor 
+
+console.log (7 < 7)
+
+console.log (7 <= 7)
+
+console.log (7 < 9)
+
+// AND && 
+
+console.log( true && true);
+
+console.log( true && false);
+
+// OR ||
+
+console.log( true || true);
+
+console.log( true || false);
+
+console.log( false || false);
+
+// Concatenacion
+
+nombre = 'Pedro';
+apellido = 'Rocha';
+
+console.log( nombre + " " + apellido )
+
+// Funcion expresada
+
+let sumar = function( NumeroA, NumeroB ) {
+    return NumeroA + NumeroB;
+}
+
+console.log(sumar(8,9));
+
+// Funcion declarada 
+
+function restar (numeroC, numeroD) {
+   return numeroC - numeroD;
+}
+
+console.log (restar(98 , 30));
---
+++ TRABAJO PRACTICO 2 'Lemon Pie 2'/app.js
@@ -0,0 +1,10 @@
+function saludar(nombre) {
+    return "hola, como estas " + nombre + "?";
+    }
+
+// ***** Aca empieza mi programa ******/
+
+const saludoAEze = saludar("Ezequiel")
+
+console.log(saludoAEze);
+
